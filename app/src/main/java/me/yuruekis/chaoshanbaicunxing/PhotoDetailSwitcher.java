146a33b88d5614e005aa3135fe14e6a320990f96
Yuru-ekis <yuruekis@gmail.com>
2019-05-17 20:01:20 +0800

    Add details for each photo.

--- app/src/main/java/me/yuruekis/chaoshanbaicunxing/PhotoDetailSwitcher.java
+++ app/src/main/java/me/yuruekis/chaoshanbaicunxing/PhotoDetailSwitcher.java
@@ -4,31 +4,299 @@ public class PhotoDetailSwitcher {
 
     //private String villageName;
 
-    private static final String T = "揭阳炮台镇桃山村：\n" +
-            "桃山村受揭阳市管辖、相邻新市村、浮岗村,物产丰富,依山傍水,气候温和,民风淳朴。揭阳市砲台镇桃山村有两座古牌坊，一座是跃禹门坊，一座是百岁坊，两坊都座落在桃山村市头谢氏家庙新华中学左右，相距百多米。跃禹门坊在揭阳市砲台镇桃山村市头新华中学前左侧，也即谢氏家庙前左侧，该坊与右侧“升平人瑞坊”（即百岁坊）并列。村庄秀丽，村民富庶，历来人文鼎盛，是揭阳市空港区炮台镇的一个有名乡村。因村中有一座小山丘，形状有似一颗巨桃，人称之为“仙桃山”，故而村庄也就定名为“桃山村”。昔时，乡村有多姓聚居，但人口不多，发展也慢。到了元朝至正八年（1348），居住于梅岗都大滘村的南宋爱国诗人谢翱，第三代孙谢东山遭寇仇杀害，宋室被毁，赖妾石氏背负刚四月的幼儿谢梅叟出走避难，至桃山，投奔潘员外家。后来梅叟便在此开基立业，成为桃山谢氏始祖。每到春节揭阳市空港经济区砲台镇桃山村有一个传统民俗就是荡秋千。桃山有三绝，首选绝色地貌，桃山属丹霞地貌，地势蜿蜒，沟深坡陡，大塬广莽，是画家写生、摄影师拍照的好去处。桃山大东沟有富硒山泉水，神话传说故且搁住不论，但桃山村大东沟山泉水是经过采集去北京科学院经过水质化验，确认其中富含人体必须的抗癌元素一” 硒\"，对人体很有益。";
+    private static final String T1 = "热情的老人老远看着我们，向我们讲解古村的风土人情";
+    private static final String T2 = "懂事的孩子“丰收”而归，载笑载言";
 
-    private static final String J = "潮州沙溪镇贾里村\n" +
-            "贾里村，谢姓。《贾里族谱》载：“我族未受姓之先，居于姜水，以姜为姓。”“申伯公定王有功，受封于谢，建国于湖南谢邑……后世子孙以国为姓，尊申伯公为谢氏鼻祖，此我祖受姓之始也。”这就是该村姓谢之由来。明洪武二十七年(公元1394年)福建莆田独树都涵首村乌衣巷谢氏肇基公来此创乡，乡名贾里，隐寓西堂贝锦之意，桥号涵头，不忘涵首旧村之义。\n" +
-            "贾里村总面积约2平方公里，720户，3269人，耕地2020亩，山地950亩，池塘，溪涧85亩，全村以农为主，是全镇主要产粮区之一，农作物有水稻、地瓜，花生、甘蔗、黄红麻、马蹄等。马蹄为本村主要经济作物，1985年，全村种植马蹄535亩，占耕地面积的百分之二十六点五，总产量17979担，总值23372元，占全村总收入的百分之二十八。\n" +
-            "贾里村地处潮、揭交界，距镇治西8里，东连五更陇村，西接刘畔村，南靠桑浦山，北与揭阳光明村相邻。\n" +
-            "贾里村昔属东莆都所辖，抗战前曾与高厦、仁里合并成立和平乡，隶属东莆区。1952年，与刘畔村合并为联和乡，1956年属高厦乡，1957年属沙溪乡，1958年后，改称贾里大队，屈金石公社，1961年后属沙溪人民公社。1983年，改称贾里乡，属沙溪区，1987年改称贾里村，属沙溪镇。\n" +
-            "贾里村包括西厝、东厝，寨内、巷头4个自然村，旧式屋宇建筑颇多，有五祠四轩一大楼四书斋。\n" +
-            "贾里村有小学一所，幼儿园一所，医疗站二问，供销分站一间，粮食加工，黄麻加工、抽纱场各一个。全村电灯照明，并饮用自来水。";
+    private static final String T3 = "肥硕的母鸡正在啄小石头，于树下悠然自得";
 
-    private static final String Q = "揭阳磐东镇乔林村：\n" +
-            "乔南村原与乔东，乔西合称古乔林村。乔南村位于广东省揭阳市东山磐东镇东部，交通便利，省道236穿境而过，商贸活跃，是五金不锈钢、玉器加工、销售业发达的工商业大村。全村占地面积3平方公里，总人口10678人，共2197户，设立16个村民小组，有初级中学1所，完全小学1所，老人活动场所2座，有市级文物保护单位勉勋公祠，舞龙烧龙为传统民间艺术活动。乔南村大力发展特色经济，因地制宜，投资建设成了乔南农贸综合市场，乔南不锈钢中心、乔南国际玉器中心等规模建筑，推动了当地经济快速发展。2009年全村集体固定资产4171万元，人均年收入5693元，村集体总收入1225万元，实现口粮供给制，每人年发口粮款400元。2003、2005、2008年先后被省，市评为文明村，现为东山区建设社会主义新农村示范点。";
+    private static final String T4 = "爷爷奶奶带着可爱的孙子在公园小憩，其乐融融";
+
+    private static final String T5 = "童年的小游戏，看着沙子堆成自己想要的形状，完成自己的作品，是多么的满足快乐啊";
+
+    private static final String T6 = "谢谢你们伴我成长，亲爱的爷爷奶奶";
+
+    private static final String T7 = "一位超级友善的老奶奶为我们指路，两人愉快地交谈着";
+
+    private static final String T8 = "勤劳的渔民开始收网，活蹦乱跳的鱼儿使人们的忙碌得到了满足";
+
+    private static final String T9 = "伫立的拐杖，蹒跚在巷间佝偻的背影，感叹岁月的沧桑与无情";
+
+    private static final String T10 = "开始尴尬又不失礼貌的搭讪，渐渐被老奶奶的热情融化，慢慢开始融洽起来";
+
+    private static final String T11 = "老屋老墙像一个古老固执的收藏家，即使周围景物千变万化，都要把历史的点点滴滴镌刻在一砖一瓦之上";
+
+    private static final String T12 = "美味的鸡蛋饼在阿姨娴熟的手艺下是那么的诱人，真让人垂涎三尺，五星好评";
+
+    private static final String T13 = "传统民居，与中国文化完美结合的建筑风格，用镜头记录它的美";
+
+    private static final String T14 = "湖水是一个画家，将岸上的传统房屋与自然美景描绘于湖面，殊不知自己也是“画中人”";
+
+    private static final String T15 = "热情的老爷爷向我们介绍古村的历史及布局，向我们展示他眼里的故土“老友”";
+
+    private static final String T16 = "岁月虽然磨掉了他的菱角，换上了青丝，却无法动摇他眼中对故土的坚定，更无法消磨心中对故土最深沉的热爱";
+
+    private static final String T17 = "戴上帽子的小光头抬头那一瞬间，心中仿佛一千万只皮卡丘在向我卖萌，简直快要萌化了";
+
+    private static final String T18 = "近代新建筑对传统建筑的冲击，历史的演替掀起太多的无奈";
+
+    private static final String T19 = "锈迹斑斑的路标接受过多少个风风雨雨的洗礼，见证过多少人与事，如果它有口，何时方能言尽";
+
+    private static final String T20 = "庄严华丽的谢氏家庙，承载着多少人文，维系着多少情思";
+
+    private static final String J1 = "小朋友是村子里欢声笑语的所在，可爱的小朋友有的好奇地看着摄影的哥哥姐姐们，有的三五成团，有的百无聊赖.";
+
+    private static final String J2 = "老奶奶欣喜地接受了我们的拍摄，在摄像机前开心得像个小孩子";
+
+    private static final String J3 = "午后的村庄，显得十分静谧空旷，岁月静好，愿这个古村也一如既往的安宁和美好";
+
+    private static final String J4 = "劳动是一首永恒的赞歌，这位老伯伯的生活因劳动而更加精彩。";
+
+    private static final String J5 = "苍老的古树见证了村子的种种变迁，从古树的沧桑中我们可以看到人们平静祥和的生活";
+
+    private static final String J6 = "高耸的屋顶上，一只白中带黑的猫傲然地坐着，回头瞥了一眼摄影者";
+
+    private static final String J7 = "平静的湖泊中，一叶小舟安静的停留在湖面，静谧的景象让观赏者浮躁的心也跟着平静下来";
+
+    private static final String J8 = "村中骑行的孩童玩得活泼快乐，给古老的村庄增添了无限的生机";
+
+    private static final String J9 = "老奶奶非常欢迎我们的到来，慈祥地与我们分享村中的老故事，使我们收益匪浅";
+
+    private static final String J10 = "回首一看，却发现小伙伴早已经摆好姿势跟在我们身后拍照";
+
+    private static final String J11 = "小朋友们对进村拍照的大哥哥大姐姐非常好奇，凑成一堆研究着大哥哥的手机";
+
+    private static final String J12 = "老奶奶在拿到我们打印出来的照片后，仔细地看着照片上面的自己，露出了开心的笑容";
+
+    private static final String J13 = "空旷的路上，老爷爷骑着同样上了年纪的自行车往前方而去";
+
+    private static final String J14 = "两位老奶奶站在一起拍照，我们为这两位多年的好友留下了一张美好的回忆";
+
+    private static final String J15 = "一群游水的鸭子打破了平静的湖水，这不就是骆宾王的诗句“白毛浮绿水”的景象吗？";
+
+    private static final String J16 = "古宅门前，老人吸着烟坐在门前，目光悠远，不知是否在盼望着什么";
+
+    private static final String J17 = "老奶奶和她的多年好友一起在看我们给她们拍的照片";
+
+    private static final String J18 = "小猫咪站在屋顶上，与身后的屋顶组成了一幅美得不可言说的水墨画";
+
+    private static final String J19 = "骑着自行车路过的老爷爷停下来与我们进行了一番有趣的交流";
+
+    private static final String J20 = "营业的叔叔在按照顾客的要求来工作，辛苦而满足";
+
+    private static final String J21 = "古村在慢慢地变老，而见证古村历史的古树也从小小的树苗长成了如今的参天大树";
+
+    private static final String J22 = "古村不仅景色美，古村里的人也特别的友好，老爷爷的笑声让我们也觉得很开心";
+
+    private static final String Q1 = "早市时刻，村中婆婆提着菜篮，拎着小袋，穿过熟悉的街头巷道，从市场徐徐归来。三两孩童或驱车在前，或坐在婆婆身上嬉戏打闹，其乐融融。";
+
+    private static final String Q2 = "路过一处小桥，在桥中停驻脚步。细雨柔柔滋润古村，微风轻轻拂过湖面，不远处妈祖庙矗立湖心招展彩旗，与涟漪泛起的湖面倒影浑然一体，小桥流水，相映成趣。";
+
+    private static final String Q3 = "戴着眼镜的和蔼爷爷对公益摄影课程组一行人的到来感到好奇，与小组同学热情相谈，向我们讲诉着村落的沧桑历史。从前的古村生活画卷在与老人的娓娓道来之中缓缓展开。";
+
+    private static final String Q4 = "一处拐角边，小孩正和母亲相互逗趣。见到小姐姐的到来，孩子丝毫不怕生的伸出稚嫩小手并露出天真可爱的笑脸，似是向我们表示欢迎。";
+
+    private static final String Q5 = "一处精美的宅邸，古朴大门与青葱绿色交相辉映，错落有致的小花点缀其上，似是睡意未醒的轻柔少女。门楣上大红灯笼的喜字分外惹眼，向行人诉说着古村生活的岁月静好和节日庆典的热闹非常。";
+
+    private static final String Q6 = "古村一隅，孩童用幼嫩的双手为家庭扛起自己的一份责任，在作坊中专心致志做着手上工作，许久不曾察觉有人前来。静静拍下，勿念勿扰。";
+
+    private static final String Q7 = "小狗追随孩童奔跑，孩童怀抱小狗打闹。看着孩童们的纯真笑脸，似是美酒般香醇，萦绕心田，情不自禁在回忆里找寻自己年幼时的那番岁月。";
+
+    private static final String Q8 = "斑驳苍白的墙壁讲诉着古村的悠久历史，其间矍铄老人驱车而过，面带笑意不紧不慢的穿梭在古村巷道。";
+
+    private static final String Q9 = "和蔼的爷爷与小组倾心相谈，向我们叙述着“悠然见南山”般恬静惬意，怡然自得的古村生活。";
+
+    private static final String Q10 = "正在交谈的老人见课程组前来问询，起身回答并热情相待。话音未落又盛情邀请同学们品尝功夫茶。小斟一杯，惊艳于馥郁茶香，品味着淳朴情怀。";
+
+    private static final String Q11 = "古树小桥流水人家，孩童与狗狗嬉笑玩耍。人与自然和谐共处，掩不住的欢快氛围跃然于照片上。";
+
+    private static final String Q12 = "爷爷不紧不慢为我们讲诉过往，向我们揭开古村人家的美好。苍老的面庞上洋溢着止不住的喜悦。";
+
+    private static final String Q13 = "爷爷与公益摄影课程组一行人交谈甚欢，带领我们回首老人的青葱岁月，并引领同学前往一旁小坐。";
+
+    private static final String Q14 = "孩子看见公益摄影课程组的同学流露出天真无邪的微笑，纯真的微笑仿佛有着抚慰一切不快的魔力，深深感染了同学们。";
+
+    private static final String Q15 = "戴着眼镜的和蔼爷爷热情相邀，向我们讲诉着村落的沧桑历史。促膝长谈间爷爷细数过往，又起身回屋拿出一张泛黄的老照片，手指划过照片上虽模糊但英气的面庞，感慨言语间也将我们一同带入了那个朴素的时代。";
+
+    private static final String Q16 = "妈妈孩子相视而笑，这便是与生俱来共通的语言。欢乐亲情一切尽在不言中。";
+
+    private static final String Q17 = "带有稚气的女孩眨巴着大眼睛，专心致志的吃着冰棒。阳光映在女孩脸庞更衬纯洁可爱。";
+
+    private static final String Q18 = "圆圆胖胖的小脸蛋上驾着小黄花似的玩具镜框，显得孩子更加天真烂漫。";
+
+    private static final String Q19 = "孩童对公益摄影课程小组感到好奇，用稚嫩眼睛向我们投来疑惑的目光。";
+
+    private static final String Q20 = "劳累于生活奔波的老人，在阳光照射下仿佛散发着耀眼光芒。";
+
+    private static final String Q21 = "古村集市旁的街头小吃，很远就可以闻到香味。老人在此摆摊为生，娴熟手法和小食美味令人赞叹不已。";
+
+    private static final String Q22 = "小溪对面，女孩对着相机示以动人微笑，活泼可爱的形象深深冲击着我们的心灵。夕阳照在人们的脸上，表情中都洋溢着生活的喜悦。";
+
+    private static final String Q23 = "小女孩甜甜圆圆的脸蛋上挂着一对水汪汪的大眼睛，扑闪着大眼向大家羞涩的打着招呼。";
+
+    private static final String Q24 = "调皮机灵的女孩正在屋内玩耍，大方的与摄影组同学互动。";
+
+    private static final String Q25 = "孩子们聚集在古屋门前空地嬉笑玩耍，看着一群追逐玩闹的孩子,使人不由自主跟着他们笑了起来。";
+
+    private static final String Q26 = "宗族祠堂中的老人坐在屋檐下悠闲的看着电视，谈天说地。";
+
+    private static final String Q27 = "躲在门后的可爱女孩起初娇羞的想要躲避拍摄，但后来还是勇敢的站了出来大方展示自己的机灵可爱。";
+
+    private static final String Q28 = "离别时刻，孩童依依不舍蜂拥向前与小组告别，狗狗跟着孩子紧随其后。为一天的公益摄影开心送行。";
 
     public static String getPhotoDetail(String photoName) {
         switch (photoName) {
-            case "T":
-                return T;
-            case "J":
-                return J;
-            case "Q":
-                return Q;
+            case "T1":
+                return T1;
+            case "T2":
+                return T2;
+            case "T3":
+                return T3;
+            case "T4":
+                return T4;
+            case "T5":
+                return T5;
+            case "T6":
+                return T6;
+            case "T7":
+                return T7;
+            case "T8":
+                return T8;
+            case "T9":
+                return T9;
+            case "T10":
+                return T10;
+            case "T11":
+                return T11;
+            case "T12":
+                return T12;
+            case "T13":
+                return T13;
+            case "T14":
+                return T14;
+            case "T15":
+                return T15;
+            case "T16":
+                return T16;
+            case "T17":
+                return T17;
+            case "T18":
+                return T18;
+            case "T19":
+                return T19;
+            case "T20":
+                return T20;
+            case "J1":
+                return J1;
+            case "J2":
+                return J2;
+            case "J3":
+                return J3;
+            case "J4":
+                return J4;
+            case "J5":
+                return J5;
+            case "J6":
+                return J6;
+            case "J7":
+                return J7;
+            case "J8":
+                return J8;
+            case "J9":
+                return J9;
+            case "J10":
+                return J10;
+            case "J11":
+                return J11;
+            case "J12":
+                return J12;
+            case "J13":
+                return J13;
+            case "J14":
+                return J14;
+            case "J15":
+                return J15;
+            case "J16":
+                return J16;
+            case "J17":
+                return J17;
+            case "J18":
+                return J18;
+            case "J19":
+                return J19;
+            case "J20":
+                return J20;
+            case "J21":
+                return J21;
+            case "J22":
+                return J22;
+            case "Q1":
+                return Q1;
+            case "Q2":
+                return Q2;
+            case "Q3":
+                return Q3;
+            case "Q4":
+                return Q4;
+            case "Q5":
+                return Q5;
+            case "Q6":
+                return Q6;
+            case "Q7":
+                return Q7;
+            case "Q8":
+                return Q8;
+            case "Q9":
+                return Q9;
+            case "Q10":
+                return Q10;
+            case "Q11":
+                return Q11;
+            case "Q12":
+                return Q12;
+            case "Q13":
+                return Q13;
+            case "Q14":
+                return Q14;
+            case "Q15":
+                return Q15;
+            case "Q16":
+                return Q16;
+            case "Q17":
+                return Q17;
+            case "Q18":
+                return Q18;
+            case "Q19":
+                return Q19;
+            case "Q20":
+                return Q20;
+            case "Q21":
+                return Q21;
+            case "Q22":
+                return Q22;
+            case "Q23":
+                return Q23;
+            case "Q24":
+                return Q24;
+            case "Q25":
+                return Q25;
+            case "Q26":
+                return Q26;
+            case "Q27":
+                return Q27;
+            case "Q28":
+                return Q28;
+
+
+
+
+
+
+
+
+
             default:
                 break;
         }
-        return T;
+        return Q1;
     }
 }
